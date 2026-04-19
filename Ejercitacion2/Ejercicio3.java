60a5e64a1a3ed49f55558eae5761cdf45ac30ffb
xiiSantixD <santiago.rodrigug@gmail.com>
2026-04-19 00:39:57 -0300

    Add files via upload

---
+++ Ejercitacion2/Ejercicio3.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Ejercicio3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] arr = new int[5];
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int min = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] < min) {
+                min = arr[i];
+            }
+        }
+
+        System.out.println("Mínimo: " + min);
+    }
+}
---
+++ Ejercitacion2/Ejercicio6.java
@@ -0,0 +1,9 @@
+
+public class Ejercicio6 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
