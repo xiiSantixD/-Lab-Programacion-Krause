cd4929e53c8aedc151d25aac5c6142edbcb8085a
xiiSantixD <santiago.rodrigug@gmail.com>
2026-04-22 20:13:18 -0300

    Implement array input and reverse print
    Added functionality to read and print an array in reverse order.

--- Ejercitacion2/Ejercicio11.java
+++ Ejercitacion2/Ejercicio11.java
@@ -1,9 +1,16 @@
+import java.util.Scanner;
 
 public class Ejercicio11 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] arr = new int[5];
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
 
-	}
-
+        for (int i = arr.length - 1; i >= 0; i--) {
+            System.out.print(arr[i] + " ");
+        }
+    }
 }
