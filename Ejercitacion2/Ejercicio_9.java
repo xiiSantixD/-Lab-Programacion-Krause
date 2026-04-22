3999a13f6e9bd01e3dcd8cb6282516dad1f54c45
xiiSantixD <santiago.rodrigug@gmail.com>
2026-04-22 20:11:28 -0300

    Add memory game functionality in Ejercicio_9.java
    Implemented a memory game that generates random numbers for the user to memorize and input.

--- Ejercitacion2/Ejercicio_9.java
+++ Ejercitacion2/Ejercicio_9.java
@@ -1,9 +1,29 @@
+import java.util.Random;
+import java.util.Scanner;
 
 public class Ejercicio_9 {
+    public static void main(String[] args) throws InterruptedException {
+        Random r = new Random();
+        Scanner sc = new Scanner(System.in);
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+        int[] arr = new int[5];
 
-	}
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = r.nextInt(10);
+            System.out.print(arr[i] + " ");
+        }
 
+        Thread.sleep(3000);
+        System.out.println("\n Memoriza y escribe:");
+
+        for (int i = 0; i < arr.length; i++) {
+            int num = sc.nextInt();
+            if (num != arr[i]) {
+                System.out.println("Perdiste");
+                return;
+            }
+        }
+
+        System.out.println("Ganaste");
+    }
 }
