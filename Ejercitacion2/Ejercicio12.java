e65cea411d4776f6ebe925e2461c74d5b39a18a7
xiiSantixD <santiago.rodrigug@gmail.com>
2026-04-22 20:13:44 -0300

    Update Ejercicio12.java

--- Ejercitacion2/Ejercicio12.java
+++ Ejercitacion2/Ejercicio12.java
@@ -1,9 +1,16 @@
-
 public class Ejercicio12 {
+    public static void main(String[] args) {
+        int[] arr = {15, 2, 8, 99, 3, 7, 1, 50};
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+        int max = arr[0];
+        int min = arr[0];
 
-	}
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > max) max = arr[i];
+            if (arr[i] < min) min = arr[i];
+        }
 
+        System.out.println("Mayor: " + max);
+        System.out.println("Menor: " + min);
+    }
 }
