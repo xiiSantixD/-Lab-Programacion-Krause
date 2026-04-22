87c85c9356d05543638f241df2f57de24a2fb6e1
xiiSantixD <santiago.rodrigug@gmail.com>
2026-04-22 20:09:29 -0300

    Implement rock-paper-scissors game logic

--- Ejercitacion2/Ejercicio7.java
+++ Ejercitacion2/Ejercicio7.java
@@ -1,9 +1,30 @@
+import java.util.Random;
+import java.util.Scanner;
 
 public class Ejercicio7 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Random r = new Random();
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+        String[] opciones = {"piedra", "papel", "tijera"};
 
-	}
+        System.out.print("Elige piedra, papel o tijera: ");
+        String usuario = sc.nextLine();
 
+        String pc = opciones[r.nextInt(3)];
+
+        System.out.println("PC eligio: " + pc);
+
+        if (usuario.equals(pc)) {
+            System.out.println("Empate");
+        } else if (
+            (usuario.equals("piedra") && pc.equals("tijera")) ||
+            (usuario.equals("papel") && pc.equals("piedra")) ||
+            (usuario.equals("tijera") && pc.equals("papel"))
+        ) {
+            System.out.println("Ganaste");
+        } else {
+            System.out.println("Perdiste");
+        }
+    }
 }
