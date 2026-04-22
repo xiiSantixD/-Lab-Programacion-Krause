e92e71fc6c2f36af637981b092180ff9dc437ef6
xiiSantixD <santiago.rodrigug@gmail.com>
2026-04-22 20:12:08 -0300

    Add word guessing game functionality
    Implemented a word guessing game where the user guesses letters to reveal a randomly selected word.

--- Ejercitacion2/Ejercicio_10.java
+++ Ejercitacion2/Ejercicio_10.java
@@ -1,9 +1,30 @@
+import java.util.Random;
+import java.util.Scanner;
 
 public class Ejercicio_10 {
+    public static void main(String[] args) {
+        String[] palabras = {"java", "programa", "computadora"};
+        Random r = new Random();
+        Scanner sc = new Scanner(System.in);
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+        String palabra = palabras[r.nextInt(3)];
+        char[] progreso = new char[palabra.length()];
 
-	}
+        for (int i = 0; i < progreso.length; i++) {
+            progreso[i] = '_';
+        }
 
+        while (!String.valueOf(progreso).equals(palabra)) {
+            System.out.println(progreso);
+            char letra = sc.next().charAt(0);
+
+            for (int i = 0; i < palabra.length(); i++) {
+                if (palabra.charAt(i) == letra) {
+                    progreso[i] = letra;
+                }
+            }
+        }
+
+        System.out.println("Ganaste");
+    }
 }
