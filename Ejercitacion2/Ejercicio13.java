957f3f96e8d37a0c18f2864133a7b199f1d73110
xiiSantixD <santiago.rodrigug@gmail.com>
2026-04-22 20:14:04 -0300

    Implement grade evaluation in Ejercicio13
    Added functionality to evaluate student grades and print results.

--- Ejercitacion2/Ejercicio13.java
+++ Ejercitacion2/Ejercicio13.java
@@ -1,9 +1,14 @@
-
 public class Ejercicio13 {
+    public static void main(String[] args) {
+        String[] nombres = {"Santiago", "Luis", "Jorge", "Mathias"};
+        double[] notas = {7.5, 4.0, 6.0, 8.2};
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+        for (int i = 0; i < nombres.length; i++) {
+            if (notas[i] >= 6) {
+                System.out.println(nombres[i] + " Aprobado");
+            } else {
+                System.out.println(nombres[i] + " Reprobado");
+            }
+        }
+    }
 }
